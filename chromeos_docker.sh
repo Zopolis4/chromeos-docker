acbc07ea19a6b7dc15632cd09ab96909115d89b7
Zopolis4 <creatorsmithmdt@gmail.com>
2023-11-09 10:34:01 +1100

    Add README and move some documentation there

--- chromeos_docker.sh
+++ chromeos_docker.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 # Copyright 2021-2023 Satadru Pramanik
 # SPDX-License-Identifier: GPL-3.0-or-later
-# chromeos_docker.sh
-# Usage:
-# REPOSITORY=YOUR_DOCKER_HUB_REPOSITORY_NAME chromeos_docker.sh recovery_file_url container_name chromeos_milestone arch
-# or if image.bin already exists this works too: 
-# chromeos_docker.sh dummy name milestone arch
-# (Default is not to delete the image after download.)
-# e.g. 
-# Example for x86_64:
-# chromeos_docker.sh https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13816.82.0_nocturne_recovery_stable-channel_mp.bin.zip nocturne 90 x86_64
-# For ChromeOS Flex:
-# chromeos_docker https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_15054.115.0_reven_recovery_stable-channel_mp-v2.bin.zip reven 106 x86_64
-# Example for armv7l:
-# chromeos_docker.sh https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13904.55.0_veyron-fievel_recovery_stable-channel_fievel-mp.bin.zip fievel 91 armv7l
-# Examples for i686:
-# chromeos_docker.sh https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_9334.72.0_x86-alex-he_recovery_stable-channel_alex-mp-v4.bin.zip alex 58 i686
-# chromeos_docker.sh https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_9334.72.0_x86-zgb-he_recovery_stable-channel_zgb-mp-v3.bin.zip zgb 58 i686
-
-# Note also you should have an account setup on docker's hub. Make sure to set that account as REPOSITORY 
-# in your environment and also make sure that you have local login from your command line enabled.
-# 
-# Alternately you can setup a local registry thus:
-# Instructions here: https://docs.docker.com/registry/deploying/
-# If you setup your registry with an ssl cert, you may have fewer problems.
-# You can set the registry URL with the REPOSITORY env variable.
-# e.g. export REPOSITORY="dockerserver:5000"
-# sudo apt install -y uidmap golang
 imageurl="${1}"
 name="${2}"
 milestone="${3}"
