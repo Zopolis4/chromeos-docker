eaeebca53dab6d843567969c8c21691db6bfa682
Zopolis4 <creatorsmithmdt@gmail.com>
2024-07-02 10:59:53 +1000

    Simplify filesystem extraction process, using virt-tar-out from libguestfs to handle much of the work.
    As a bonus, this properly preserves permissions and uid bits, fixing the functionality of the images and removing the need for manually trying to restore the permissions.

--- chromeos_docker.sh
+++ chromeos_docker.sh
@@ -18,23 +18,14 @@ setup_base () {
     # Download image
     curl --progress-bar --retry 3 -Lf "${url}" -o "${cached_image}".zip || ( echo "Download failed" && kill $$ )
 
-    # Extract image
-
-    # Extract the various layers of archives until we get to the root filesystem
-    # 7zip does not currently support extracting nested archives in one command, or extracting from stdin
+    # Unzip the image
     7z x "${cached_image}".zip -so > "${cached_image}".bin
-    7z x "${cached_image}".bin 2.ROOT-A.img
 
-    # Additionally, 7zip does not currently preserve the required permissions or uid bits when extracting
-    # 7z x -snld 2.ROOT-A.img -o"${cached_image}"
-#     virt-copy-out -a 2.ROOT-A.img / ${cached_image}
-
-    # Tar the unpacked filesystem for importing into docker
-    (cd "${cached_image}" && tar -pcf ../"${cached_image}".tar .)
+    # Extract the filesystem directly into a tarball for importing into docker
+    virt-tar-out -a "${cached_image}".bin / "${cached_image}".tar
 
     # Clean up after ourselves
-    rm -f "${cached_image}".zip "${cached_image}".bin 2.ROOT-A.img
-    rm -rf "${cached_image}"
+    rm -f "${cached_image}".zip "${cached_image}".bin
 
     # If we don't already have the image cached, we don't already have it imported into docker and pushed to the repository
     docker import "${cached_image}".tar --platform "${PLATFORM}" "${REPOSITORY}"/crewbase:"${name}".m"${milestone}"
